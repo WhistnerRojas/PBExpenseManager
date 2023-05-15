d790f3f058d7ab9e204cd6b4d0fac59748c8bfd4
Whistner Rojas <whistner.rojas.fg@gmail.com>
2023-05-15 08:01:11 +0800

    added my expenses

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,47 @@
 
             <div class="card-body">
 
+                <div class="row">
+                    <div class="col">
+                        <?php
+                            foreach($viewExpenses['expenses'] as $expeses){
+                                echo "<h5>$expeses->expense_category = &#8369;$expeses->amount</h5>";
+                            }
+                        ?>
+                    </div>
+                    <div class="col">
+                        <div id="chartContainer"></div>
+                    </div>
+                </div>
+                <script src="https://code.highcharts.com/highcharts.js"></script>
+                <script>
+                    document.addEventListener('DOMContentLoaded', function() {
+                        // Data for the chart (replace with your own data)
+                        var data = [<?php 
+                            foreach($viewExpenses['expenses'] as $expeses){
+                                echo "['$expeses->expense_category', $expeses->amount],";
+                            }
+                        ?>];
+                        // var data = [
+                        //     ['Category 1', 20],
+                        //     ['Category 2', 30],
+                        //     ['Category 3', 50]
+                        // ];
+                        // Create the chart
+                        Highcharts.chart('chartContainer', {
+                            chart: {
+                                type: 'pie'
+                            },
+                            title: {
+                                text: 'Pie Chart'
+                            },
+                            series: [{
+                                name: 'Categories',
+                                data: data
+                            }]
+                        });
+                    });
+                </script>
 
             </div>
 
